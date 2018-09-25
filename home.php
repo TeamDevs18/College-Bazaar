e8814598c895f28fec820e67199a1bd418ce4832
Alissa Barnard <ambarnar@oakland.edu>
2018-09-24 21:04:05 -0400

    Add files via upload

---
+++ home.php
@@ -0,0 +1,4 @@
+<h1>Welcome to the College Bazaar</h1>
+<p>
+The College Bazaar is a place to sell items you do not need anymore, or buy items from other college students. Here you can also send messages to other college students looking to barter services.
+</p>
